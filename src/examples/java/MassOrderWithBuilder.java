379a3931d8967a3521c5f36dd05d01b46f325df5
Michael Barker <michael.barker@lmax.com>
2014-06-19 12:13:23 +1200

    Fixed small build errors/warnings and reformatted.

--- src/examples/java/MassOrderWithBuilder.java
+++ src/examples/java/MassOrderWithBuilder.java
@@ -9,12 +9,13 @@ import org.ObjectLayout.CtorAndArgs;
 import org.ObjectLayout.CtorAndArgsProvider;
 import org.ObjectLayout.StructuredArray;
 
-public class MassOrderWithBuilder extends StructuredArray<SimpleOrderWithBuilder> {
+public class MassOrderWithBuilder extends
+        StructuredArray<SimpleOrderWithBuilder> {
 
     private final long accountId;
     private final String instructionId;
     private final OrderType orderType;
-	private long instrumentId;
+    private long instrumentId;
 
     MassOrderWithBuilder(final Builder builder) {
         this.accountId = builder.accountId;
@@ -36,142 +37,147 @@ public class MassOrderWithBuilder extends StructuredArray<SimpleOrderWithBuilder
     }
 
     public long getInstrumentId() {
-	return instrumentId;
+        return instrumentId;
     }
 
     public static enum OrderType {
-	MARKET, LIMIT
+        MARKET, LIMIT
     }
 
     private static final ThreadLocal<Builder> BUILDER = new ThreadLocal<MassOrderWithBuilder.Builder>() {
-	protected Builder initialValue() {
-		try {
-				return new Builder();
-			} catch (NoSuchMethodException e) {
-				throw new RuntimeException(e);
-			}
-	};
+        protected Builder initialValue() {
+            try {
+                return new Builder();
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        };
     };
 
     public static Builder builder() {
-	return BUILDER.get();
+        return BUILDER.get();
     }
 
-    public static class Builder extends CtorAndArgsProvider<SimpleOrderWithBuilder> {
+    public static class Builder extends
+            CtorAndArgsProvider<SimpleOrderWithBuilder> {
 
         private static CtorAndArgs<MassOrderWithBuilder> massOrderCtorAndArgs() {
             try {
-			final Class[] massOrderConstructorArgTypes = {Builder.class};
-			final Constructor<MassOrderWithBuilder> massOrderConstructor;
-			massOrderConstructor = MassOrderWithBuilder.class.getConstructor(massOrderConstructorArgTypes);
-			return new CtorAndArgs<MassOrderWithBuilder>(massOrderConstructor, (Object[]) null);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+                final Class[] massOrderConstructorArgTypes = { Builder.class };
+                final Constructor<MassOrderWithBuilder> massOrderConstructor;
+                massOrderConstructor = MassOrderWithBuilder.class
+                        .getConstructor(massOrderConstructorArgTypes);
+                return new CtorAndArgs<MassOrderWithBuilder>(
+                        massOrderConstructor, (Object[]) null);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
         private static CtorAndArgs<SimpleOrderWithBuilder> simpleOrderArgs() {
-		try {
-			final Constructor<SimpleOrderWithBuilder> simpleOrderConstructor =
-					SimpleOrderWithBuilder.class.getConstructor(Builder.class, long.class, long.class);
-			return new CtorAndArgs<SimpleOrderWithBuilder>(simpleOrderConstructor, null, 0L, 0L);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+            try {
+                final Constructor<SimpleOrderWithBuilder> simpleOrderConstructor = SimpleOrderWithBuilder.class
+                        .getConstructor(Builder.class, long.class, long.class);
+                return new CtorAndArgs<SimpleOrderWithBuilder>(
+                        simpleOrderConstructor, null, 0L, 0L);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
-		private static final int MAX_ORDERS_PER_SIDE = 6;
+        private static final int MAX_ORDERS_PER_SIDE = 6;
 
-		long accountId;
-	long instrumentId;
-	String instructionId;
-	OrderType orderType;
+        long accountId;
+        long instrumentId;
+        String instructionId;
+        OrderType orderType;
 
-	private int bidIndex;
-	private long[] bidPrices = new long[MAX_ORDERS_PER_SIDE];
-	private long[] bidQuantities = new long[MAX_ORDERS_PER_SIDE];
+        private int bidIndex;
+        private long[] bidPrices = new long[MAX_ORDERS_PER_SIDE];
+        private long[] bidQuantities = new long[MAX_ORDERS_PER_SIDE];
 
-	private int askIndex;
-	private long[] askPrices = new long[MAX_ORDERS_PER_SIDE];
-	private long[] askQuantities = new long[MAX_ORDERS_PER_SIDE];
+        private int askIndex;
+        private long[] askPrices = new long[MAX_ORDERS_PER_SIDE];
+        private long[] askQuantities = new long[MAX_ORDERS_PER_SIDE];
 
-	private final CtorAndArgs<MassOrderWithBuilder> massOrderArgs = massOrderCtorAndArgs();
-	private CtorAndArgs<SimpleOrderWithBuilder> simpleOrderArgs;
+        private final CtorAndArgs<MassOrderWithBuilder> massOrderArgs = massOrderCtorAndArgs();
+        private CtorAndArgs<SimpleOrderWithBuilder> simpleOrderArgs;
 
-	public Builder() throws NoSuchMethodException {
-		super(SimpleOrderWithBuilder.class);
-		massOrderArgs.setArgs(this);
-	}
+        public Builder() throws NoSuchMethodException {
+            super(SimpleOrderWithBuilder.class);
+            massOrderArgs.setArgs(this);
+        }
 
-	public Builder accountId(long accountId) {
-		this.accountId = accountId;
-		return this;
-	}
+        public Builder accountId(long accountId) {
+            this.accountId = accountId;
+            return this;
+        }
 
-	public Builder instrumentId(long instrumentId) {
-		this.instrumentId = instrumentId;
-		return this;
-	}
+        public Builder instrumentId(long instrumentId) {
+            this.instrumentId = instrumentId;
+            return this;
+        }
 
-	public Builder instructionId(String instructionId) {
-		this.instructionId = instructionId;
-		return this;
-	}
+        public Builder instructionId(String instructionId) {
+            this.instructionId = instructionId;
+            return this;
+        }
 
-	public Builder orderType(OrderType orderType) {
-		this.orderType = orderType;
-		return this;
-	}
+        public Builder orderType(OrderType orderType) {
+            this.orderType = orderType;
+            return this;
+        }
 
-	public Builder addBid(long price, long quantity) {
-		bidPrices[bidIndex] = price;
-		bidQuantities[bidIndex] = quantity;
-		bidIndex++;
-		return this;
-	}
+        public Builder addBid(long price, long quantity) {
+            bidPrices[bidIndex] = price;
+            bidQuantities[bidIndex] = quantity;
+            bidIndex++;
+            return this;
+        }
 
-	public Builder addAsk(long price, long quantity) {
-		askPrices[askIndex] = price;
-		askQuantities[askIndex] = quantity;
-		askIndex++;
-		return this;
-	}
+        public Builder addAsk(long price, long quantity) {
+            askPrices[askIndex] = price;
+            askQuantities[askIndex] = quantity;
+            askIndex++;
+            return this;
+        }
 
-	@Override
-	public CtorAndArgs<SimpleOrderWithBuilder> getForIndices(long[] indices)
-			throws NoSuchMethodException {
+        @Override
+        public CtorAndArgs<SimpleOrderWithBuilder> getForIndices(
+                long... indices) throws NoSuchMethodException {
 
-		CtorAndArgs<SimpleOrderWithBuilder> args = simpleOrderArgs != null ? simpleOrderArgs : simpleOrderArgs();
+            CtorAndArgs<SimpleOrderWithBuilder> args = 
+                    simpleOrderArgs != null ? simpleOrderArgs : simpleOrderArgs();
 
-		long index = indices[0];
+            long index = indices[0];
 
-		if (index > bidIndex + askIndex) {
-			throw new IllegalArgumentException();
-		}
+            if (index > bidIndex + askIndex) {
+                throw new IllegalArgumentException();
+            }
 
-		int i = (int) index;
-		if (i < bidIndex) {
-			args.setArgs(this, bidPrices[i], bidQuantities[i]);
-		} else {
-			i = i - bidIndex;
-			args.setArgs(this, askPrices[i], askQuantities[i]);
-		}
+            int i = (int) index;
+            if (i < bidIndex) {
+                args.setArgs(this, bidPrices[i], bidQuantities[i]);
+            } else {
+                i = i - bidIndex;
+                args.setArgs(this, askPrices[i], askQuantities[i]);
+            }
 
-		return args;
-	}
+            return args;
+        }
 
-	@SuppressWarnings("unchecked")
-		@Override
-	public void recycle(CtorAndArgs ctorAndArgs) {
-		this.simpleOrderArgs = ctorAndArgs;
-	}
+        @Override
+        public void recycle(CtorAndArgs<SimpleOrderWithBuilder> ctorAndArgs) {
+            this.simpleOrderArgs = ctorAndArgs;
+        }
 
-	public MassOrderWithBuilder newInstance() {
+        public MassOrderWithBuilder newInstance() {
 
-		// TODO: Order validation and sorting by price.
+            // TODO: Order validation and sorting by price.
 
-		long length = bidIndex + askIndex;
-		return (MassOrderWithBuilder) StructuredArray.newSubclassInstance(massOrderArgs, this, length);
-	}
+            long length = bidIndex + askIndex;
+            return (MassOrderWithBuilder) StructuredArray.newSubclassInstance(
+                    massOrderArgs, this, length);
+        }
     }
 }
--- src/examples/java/SimpleOrderWithBuilder.java
+++ src/examples/java/SimpleOrderWithBuilder.java
@@ -3,59 +3,58 @@
  * as explained at http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-
-
 public class SimpleOrderWithBuilder {
-	private final long instrumentId;
-	private final long accountId;
-	private final MassOrderWithBuilder.OrderType orderType;
-	private final long price;
-	private final long quantity;
+    private final long instrumentId;
+    private final long accountId;
+    private final MassOrderWithBuilder.OrderType orderType;
+    private final long price;
+    private final long quantity;
 
-	private long cancelledQuantity = 0;
-	private long filledQuantity = 0;
+    private long cancelledQuantity = 0;
+    private long filledQuantity = 0;
 
-	public SimpleOrderWithBuilder(MassOrderWithBuilder.Builder builder, long price, long quantity) {
-		this.instrumentId = builder.instrumentId;
-		this.accountId = builder.accountId;
-		this.orderType = builder.orderType;
-		this.price = price;
-		this.quantity = quantity;
-	}
+    public SimpleOrderWithBuilder(MassOrderWithBuilder.Builder builder,
+            long price, long quantity) {
+        this.instrumentId = builder.instrumentId;
+        this.accountId = builder.accountId;
+        this.orderType = builder.orderType;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
-	public long getCancelledQuantity() {
-		return this.cancelledQuantity;
-	}
+    public long getCancelledQuantity() {
+        return this.cancelledQuantity;
+    }
 
-	public void updateCancelled(long delta) {
-		this.cancelledQuantity -= delta;
-	}
+    public void updateCancelled(long delta) {
+        this.cancelledQuantity -= delta;
+    }
 
-	public long getFilledQuantity() {
-		return this.filledQuantity;
-	}
+    public long getFilledQuantity() {
+        return this.filledQuantity;
+    }
 
-	public void updateFill(long matchedQuantity) {
-		this.filledQuantity -= matchedQuantity;
-	}
+    public void updateFill(long matchedQuantity) {
+        this.filledQuantity -= matchedQuantity;
+    }
 
-	public long getInstrumentId() {
-		return this.instrumentId;
-	}
+    public long getInstrumentId() {
+        return this.instrumentId;
+    }
 
-	public long getAccountId() {
-		return this.accountId;
-	}
+    public long getAccountId() {
+        return this.accountId;
+    }
 
-	public MassOrderWithBuilder.OrderType getOrderType() {
-		return this.orderType;
-	}
+    public MassOrderWithBuilder.OrderType getOrderType() {
+        return this.orderType;
+    }
 
-	public long getPrice() {
-		return this.price;
-	}
+    public long getPrice() {
+        return this.price;
+    }
 
-	public long getQuantity() {
-		return this.quantity;
-	}
+    public long getQuantity() {
+        return this.quantity;
+    }
 }
